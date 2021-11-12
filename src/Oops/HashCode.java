584b6458323adba2e1bf13dce1ce2924bee59e68
Rahul Chaudhary <ch.rahuljaat777@gmail.com>
2021-11-13 00:55:50 +0530

    Hash code from JVM

---
+++ src/Oops/HashCode.java
@@ -0,0 +1,14 @@
+package oops;
+
+public class HashCode {
+    int a;
+    // other Statements
+}
+class HashCodeMain{
+    public static void main(String[] args) {
+        // If we did not give any value in class Then constructor call it give HashCode value
+        HashCode obj = new HashCode();
+        System.out.println(obj);
+
+    }
+}
